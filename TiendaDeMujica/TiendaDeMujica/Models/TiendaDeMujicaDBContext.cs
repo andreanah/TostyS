ede19742dba63a3d2cce584289df609334370636
Edgar Niño <edgarnio@live.com>
2021-03-10 16:23:33 -0600

    Proyecto Inicial

---
+++ TiendaDeMujica/TiendaDeMujica/Models/TiendaDeMujicaDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaDeMujica.Models
+{
+    public class TiendaDeMujicaDBContext : DbContext
+    {
+        //public DbSet<> { get; set; }
+
+        public TiendaDeMujicaDBContext()
+        {
+            
+        }
+
+        public TiendaDeMujicaDBContext(DbContextOptions<TiendaDeMujicaDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<>(entity =>
+            //{
+
+            //});
+        }
+    }
+}
