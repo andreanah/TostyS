70100246c2f6fcb4c9addd28e96a23357ccf7b69
AremZ <61175996+AremZ@users.noreply.github.com>
2021-03-10 18:49:34 -0600

    Format, Genre, ProductFormat
    Agregados.

---
+++ TiendaDeMujica/TiendaDeMujica/Models/ProductFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaDeMujica.Models
+{
+    public class ProductFormat
+    {
+        public int Id { get; set; }
+        public int IdProduct { get; set; }
+        public int IdFormat { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Format Format { get; set; }
+    }
+}
