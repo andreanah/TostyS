af177e3f61c9b1f23f84ff798d7e8a0331451be5
Edgar Niño <edgarnio@live.com>
2021-04-21 16:52:34 -0500

    Se agregaron los DbSet y las tablas necesarias para ASPNetUsers

--- TiendaDeMujica/TiendaDeMujica/Models/TiendaDeMujicaDBContext.cs
+++ TiendaDeMujica/TiendaDeMujica/Models/TiendaDeMujicaDBContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +8,22 @@ using System.Threading.Tasks;
 
 namespace TiendaDeMujica.Models
 {
-    public class TiendaDeMujicaDBContext : DbContext
+    public class TiendaDeMujicaDBContext : IdentityDbContext<User>
     {
         //public DbSet<> { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<ArtistProduct> ArtistProduct { get; set; }
+        public DbSet<CreditCard> CreditCard { get; set; }
         public DbSet<Format> Format { get; set; }
         public DbSet<Genre> Genre { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderProduct> OrderProduct { get; set; }
+        public DbSet<Photos> Photos { get; set; }
+        public DbSet<Product> Product { get; set; }
         public DbSet<ProductFormat> ProductFormat { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public DbSet<User> User { get; set; }
 
         public TiendaDeMujicaDBContext()
         { 
@@ -45,8 +57,8 @@ namespace TiendaDeMujica.Models
                     .HasMaxLength(20)
                     .IsRequired()
                     .IsUnicode(false);
-                entity.Property(e => e.Username)
-                    .HasMaxLength(15)
+                entity.Property(e => e.IdUser)
+                    .HasMaxLength(450)
                     .IsRequired()
                     .IsUnicode(false);
 
@@ -105,8 +117,8 @@ namespace TiendaDeMujica.Models
                     .HasColumnType("date")
                     .IsRequired();
 
-                entity.Property(e => e.Username)
-                    .HasMaxLength(15)
+                entity.Property(e => e.IdUser)
+                    .HasMaxLength(450)
                     .IsRequired()
                     .IsUnicode(false);
 
@@ -150,10 +162,10 @@ namespace TiendaDeMujica.Models
 
                 entity.Property(e => e.IdAddress)
                     .IsRequired();
-                entity.Property(e => e.Username)
-                    .HasMaxLength(30)
-                    .IsUnicode(false)
-                    .IsRequired();
+                entity.Property(e => e.IdUser)
+                    .HasMaxLength(450)
+                    .IsRequired()
+                    .IsUnicode(false);
 
                 entity.HasOne(e => e.User)
                     .WithMany(y => y.Order)
@@ -246,10 +258,10 @@ namespace TiendaDeMujica.Models
                     .HasMaxLength(50)
                     .IsUnicode(false)
                     .IsRequired();
-                entity.Property(e => e.Username)
-                    .HasMaxLength(15)
-                    .IsUnicode(false)
-                    .IsRequired();
+                entity.Property(e => e.IdUser)
+                    .HasMaxLength(450)
+                    .IsRequired()
+                    .IsUnicode(false);
 
                 entity.HasOne(e => e.Product)
                    .WithMany(y => y.ShoppingCart)
@@ -259,33 +271,50 @@ namespace TiendaDeMujica.Models
                    .WithMany(y => y.ShoppingCart)
                    .HasForeignKey("FK_ShoppingCartUser");
             });
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.Username);
 
-                entity.Property(e => e.Username)
-                    .HasMaxLength(15)
-                    .IsUnicode(false)
-                    .IsRequired();
-                entity.Property(e => e.Name)
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("AspNetUsers");
+                user.HasKey(e => e.Id);
+
+                user.Property(e => e.Name)
                     .HasMaxLength(50)
                     .IsUnicode(false)
                     .IsRequired();
-                entity.Property(e => e.Password)
-                    .HasMaxLength(15)
-                    .IsUnicode(false)
-                    .IsRequired();
-                entity.Property(e => e.Email)
-                    .HasMaxLength(30)
-                    .IsUnicode(false)
-                    .IsRequired();
-                entity.Property(e => e.PhoneNumber)
-                    .HasMaxLength(10)
-                    .IsUnicode(false)
-                    .IsRequired();
-                entity.Property(e => e.Active)
+                user.Property(e => e.Active)
                     .HasColumnType("bit")
                     .IsRequired();
+            });
+            
+            modelBuilder.Entity<IdentityRole<string>>(entity =>
+            {
+                entity.ToTable("AspNetRoles");
+                entity.HasKey(e => e.Id);
+            });
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("AspNetUserLogins");
+                entity.HasKey("LoginProvider", "ProviderKey");
+            });
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("AspNetRoleClaims");
+                entity.HasKey(e => e.Id);
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("AspNetUserRoles");
+                entity.HasKey("UserId", "RoleId");
+            });
+            modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("AspNetUserTokens");
+                entity.HasKey("UserId", "LoginProvider");
+            });
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("AspNetUserClaims");
+                entity.HasKey(e => e.Id);
             });
 
         }
