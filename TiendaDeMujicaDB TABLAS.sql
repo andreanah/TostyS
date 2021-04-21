c13c89ab38168acb5687a246a64b64b2aef45d61
Edgar Niño <edgarnio@live.com>
2021-04-21 16:54:40 -0500

    Se agregaron las tablas de ASPNetUsers y se corrigieron los ForeignKeys
    Se cambio Username por IdUser y las tablas
    [AspNetRoleClaims]
    [AspNetRoles]
    [AspNetUserClaims]
    [AspNetUserLogins]
    [AspNetUserRoles]
    [AspNetUsers]
    [AspNetUserTokens]

--- TiendaDeMujicaDB TABLAS.sql
+++ TiendaDeMujicaDB TABLAS.sql
@@ -19,27 +19,219 @@ Creación de Tablas [User][Format][Genre][Artist][Product][ProductFormat][Shoppin
 
 Fecha de creación: 2021-02-01
 
+-Se borro la tabla User por ASPNetUsers
+
+-Se agrego las tablas [AspNetRoleClaims][AspNetRoles][AspNetUserClaims][AspNetUserLogins][AspNetUserRoles][AspNetUsers][AspNetUserTokens]
+
+-Foreign Keys corregidos en [ShoppingCart][Address][Order][CreditCard] se cambio User a ASPNetUsers y el nombre de las variables Username por IdUser
+
+Fecha de actualización: 2021-04-21
 */
+
 CREATE DATABASE TiendaDeMujicaDB;
 GO
 
-USE TiendaDeMujicaDB;
+/***************************
+TABLAS ASPNET
+****************************/
+
+
+USE [TiendaDeMujicaDB]
 GO
 
-IF OBJECT_ID('dbo.[User]', 'U') IS NOT NULL
-	DROP TABLE dbo.[User];
+
+/****** Object:  Table [dbo].[AspNetRoleClaims]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+
+SET ANSI_NULLS ON
 GO
-CREATE TABLE [User](
-	Username VARCHAR(15) NOT NULL,
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.[AspNetRoleClaims]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetRoleClaims];
+GO
+CREATE TABLE [dbo].[AspNetRoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetRoles]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.[AspNetRoles]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetRoles];
+GO
+CREATE TABLE [dbo].[AspNetRoles](
+	[Id] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](256) NULL,
+	[NormalizedName] [nvarchar](256) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetRoles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserClaims]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('dbo.[AspNetUserClaims]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetUserClaims];
+GO
+CREATE TABLE [dbo].[AspNetUserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserLogins]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('dbo.[AspNetUserLogins]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetUserLogins];
+GO
+CREATE TABLE [dbo].[AspNetUserLogins](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[ProviderDisplayName] [nvarchar](max) NULL,
+	[UserId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUserRoles]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('dbo.[AspNetUserRoles]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetUserRoles];
+GO
+CREATE TABLE [dbo].[AspNetUserRoles](
+	[UserId] [nvarchar](450) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+ CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AspNetUsers]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.[AspNetUsers]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetUsers];
+GO
+CREATE TABLE [dbo].[AspNetUsers](
+	[Id] [nvarchar](450) NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[NormalizedUserName] [nvarchar](256) NULL,
+	[Email] [nvarchar](256) NULL,
+	[NormalizedEmail] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
 	[Name] VARCHAR(50) NOT NULL,
-	[Password] VARCHAR(15) NOT NULL,
-	Email VARCHAR(30) NOT NULL,
-	PhoneNumber VARCHAR(10) NULL,
-	Active BIT NOT NULL,
-
-	PRIMARY KEY(Username),
-);
+	[Active] BIT NOT NULL,
+ CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+/****** Object:  Table [dbo].[AspNetUserTokens]    Script Date: 27/03/2021 11:05:23 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF OBJECT_ID('dbo.[AspNetUserTokens]', 'U') IS NOT NULL
+	DROP TABLE dbo.[AspNetUsers];
+GO
+CREATE TABLE [dbo].[AspNetUserTokens](
+	[UserId] [nvarchar](450) NOT NULL,
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](128) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC,
+	[LoginProvider] ASC,
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AspNetRoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetRoleClaims] CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+GO
+ALTER TABLE [dbo].[AspNetUserClaims]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserClaims] CHECK CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserLogins]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserLogins] CHECK CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[AspNetRoles] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId]
+GO
+ALTER TABLE [dbo].[AspNetUserRoles]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserRoles] CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId]
+GO
+ALTER TABLE [dbo].[AspNetUserTokens]  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES [dbo].[AspNetUsers] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[AspNetUserTokens] CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]
+GO
+
+/***************************
+TABLAS Base de Datos
+****************************/
 
 IF OBJECT_ID('dbo.[Format]', 'U') IS NOT NULL
 	DROP TABLE dbo.[Format];
@@ -123,13 +315,13 @@ CREATE TABLE ShoppingCart(
 	Id INT NOT NULL IDENTITY(1,1),
 
 	IdProduct INT NOT NULL,
-	Username VARCHAR(15) NOT NULL,
+	[IdUser] [nvarchar](450) NOT NULL,
 
 	CONSTRAINT FK_ShoppingCartProduct
 	FOREIGN KEY (IdProduct) REFERENCES Product(Id),
 
 	CONSTRAINT FK_ShoppingCartUser
-	FOREIGN KEY (Username) REFERENCES [User](Username),
+	FOREIGN KEY (IdUser) REFERENCES [AspNetUsers](Id),
 
 	PRIMARY KEY(Id),
 );
@@ -181,10 +373,10 @@ CREATE TABLE [Address](
 	Country VARCHAR(20) NOT NULL,
 	Suburb VARCHAR(20) NOT NULL,
 	
-	Username VARCHAR(15) NOT NULL,
+	[IdUser] [nvarchar](450) NOT NULL,
 
 	CONSTRAINT FK_AddressUser
-	FOREIGN KEY (Username) REFERENCES [User](Username),
+	FOREIGN KEY (IdUser) REFERENCES [AspNetUsers](Id),
 	
 	PRIMARY KEY (Id),
 );
@@ -199,10 +391,10 @@ CREATE TABLE [Order](
 	[Status] VARCHAR(30)NOT NULL,
 	
 	IdAddress INT NOT NULL,
-	Username VARCHAR(15) NOT NULL,
+	[IdUser] [nvarchar](450) NOT NULL,
 	
 	CONSTRAINT FK_OrderUser
-	FOREIGN KEY (Username) REFERENCES [User](Username),
+	FOREIGN KEY (IdUser) REFERENCES [AspNetUsers](Id),
 	
 	CONSTRAINT FK_OrderAddress
 	FOREIGN KEY (IdAddress) REFERENCES [Address](Id),
@@ -238,10 +430,10 @@ CREATE TABLE CreditCard(
 	DateBirth DATE NOT NULL,
 	CreditCardNumber VARCHAR(16) NOT NULL,
 
-	Username VARCHAR(15) NOT NULL,
+	[IdUser] [nvarchar](450) NOT NULL,
 
 	CONSTRAINT FK_CreditCardUser
-	FOREIGN KEY (Username) REFERENCES [User](Username),
+	FOREIGN KEY (IdUser) REFERENCES [AspNetUsers](Id),
 
 	PRIMARY KEY (Id)
 ); 
