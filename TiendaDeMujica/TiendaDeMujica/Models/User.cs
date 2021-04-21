fab707ade74834bcded31f9dddb9220fb7ec9a8e
Edgar Niño <edgarnio@live.com>
2021-04-21 16:49:19 -0500

    Se le agrego IdentityUser a User
    Se borro Username, Password, Email y PhoneNumber

--- TiendaDeMujica/TiendaDeMujica/Models/User.cs
+++ TiendaDeMujica/TiendaDeMujica/Models/User.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TiendaDeMujica.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public string Username { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string? PhoneNumber { get; set; }
         public bool Active { get; set; }
 
         public virtual ICollection<Address> Address { get; set; }
