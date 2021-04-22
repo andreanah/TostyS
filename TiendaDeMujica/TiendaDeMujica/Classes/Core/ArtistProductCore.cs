fdbae267ed0086b7dab1c4e2a16cf274dcd40fb9
AremZ <61175996+AremZ@users.noreply.github.com>
2021-04-22 13:50:52 -0500

    Agregado Controller y Core de ArtistProduct

---
+++ TiendaDeMujica/TiendaDeMujica/Classes/Core/ArtistProductCore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaDeMujica.Models;
+
+namespace TiendaDeMujica.Classes.Core
+{
+    public class ArtistProductCore
+    {
+        TiendaDeMujicaDBContext dBContext;
+        public ArtistProductCore(TiendaDeMujicaDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public List<ArtistProduct> Get()
+        {
+            try
+            {
+                return (from ap in dBContext.ArtistProduct
+                        select ap).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<ArtistProduct> Get(int id)
+        {
+            try
+            {
+                return (from ap in dBContext.ArtistProduct
+                        where ap.Id == id
+                        select ap).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Create(ArtistProduct artistProduct)
+        {
+            try
+            {
+                bool validArtistProduct = Validate(artistProduct);
+                if (validArtistProduct)
+                {
+                    dBContext.Add(artistProduct);
+                    dBContext.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public bool Validate(ArtistProduct artistProduct)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(artistProduct.IdArtist.ToString()) || string.IsNullOrEmpty(artistProduct.IdProduct.ToString()))
+                    return false;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Update(ArtistProduct artistProduct, int id)
+        {
+            try
+            {
+                bool validArtistProduct = Validate(artistProduct);
+
+                if (validArtistProduct)
+                {
+                    bool existingArtistProduct = dBContext.ArtistProduct.Any(artistProduct => artistProduct.Id == id);
+                    if (existingArtistProduct)
+                    {
+                        artistProduct.Id = id;
+
+                        dBContext.Update(artistProduct);
+                        dBContext.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ TiendaDeMujica/TiendaDeMujica/Controllers/ArtistProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TiendaDeMujica.Classes.Core;
+using TiendaDeMujica.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TiendaDeMujica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistProductController : ControllerBase
+    {
+        private TiendaDeMujicaDBContext dbContext;
+
+        public ArtistProductController(TiendaDeMujicaDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                ArtistProductCore artistProductCore = new ArtistProductCore(dbContext);
+                return Ok(artistProductCore.Get());
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            try
+            {
+                ArtistProductCore artistProductCore = new ArtistProductCore(dbContext);
+                return Ok(artistProductCore.Get(id));
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ArtistProduct artistProduct)
+        {
+            try
+            {
+                ArtistProductCore artistProductCore = new ArtistProductCore(dbContext);
+
+                artistProductCore.Create(artistProduct);
+                return Ok("Added new artist-product successfully!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] ArtistProduct artistProduct, [FromRoute] int id)
+        {
+            try
+            {
+                ArtistProductCore artistProductCore = new ArtistProductCore(dbContext);
+
+                artistProductCore.Update(artistProduct, id);
+                return Ok("Artist-product successfully edited!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
