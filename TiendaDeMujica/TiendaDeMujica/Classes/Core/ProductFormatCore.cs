e0c1f701ac4fb65b11501336b42c7066091aa4ad
AremZ <61175996+AremZ@users.noreply.github.com>
2021-04-22 13:35:09 -0500

    Agregado Controller y Core de ProductFormat

---
+++ TiendaDeMujica/TiendaDeMujica/Classes/Core/ProductFormatCore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaDeMujica.Models;
+
+namespace TiendaDeMujica.Classes.Core
+{
+    public class ProductFormatCore
+    {
+        TiendaDeMujicaDBContext dBContext;
+        public ProductFormatCore(TiendaDeMujicaDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public List<ProductFormat> Get()
+        {
+            try
+            {
+                return (from pf in dBContext.ProductFormat
+                        select pf).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<ProductFormat> Get(int id)
+        {
+            try
+            {
+                return (from pf in dBContext.ProductFormat
+                        where pf.Id == id
+                        select pf).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Create(ProductFormat productFormat)
+        {
+            try
+            {
+                bool validProductFormat = Validate(productFormat);
+                if (validProductFormat)
+                {
+                    dBContext.Add(productFormat);
+                    dBContext.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public bool Validate(ProductFormat productFormat)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(productFormat.IdProduct.ToString()) || string.IsNullOrEmpty(productFormat.IdFormat.ToString()))
+                    return false;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Update(ProductFormat productFormat, int id)
+        {
+            try
+            {
+                bool validProductFormat = Validate(productFormat);
+
+                if (validProductFormat)
+                {
+                    bool existingProductFormat = dBContext.ProductFormat.Any(productFormat => productFormat.Id == id);
+                    if (existingProductFormat)
+                    {
+                        productFormat.Id = id;
+
+                        dBContext.Update(productFormat);
+                        dBContext.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ TiendaDeMujica/TiendaDeMujica/Controllers/ProductFormatController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TiendaDeMujica.Classes.Core;
+using TiendaDeMujica.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TiendaDeMujica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductFormatController : ControllerBase
+    {
+        private TiendaDeMujicaDBContext dbContext;
+
+        public ProductFormatController(TiendaDeMujicaDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                ProductFormatCore productFormatCore = new ProductFormatCore(dbContext);
+                return Ok(productFormatCore.Get());
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            try
+            {
+                ProductFormatCore productFormatCore = new ProductFormatCore(dbContext);
+                return Ok(productFormatCore.Get(id));
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ProductFormat productFormat)
+        {
+            try
+            {
+                ProductFormatCore productFormatCore = new ProductFormatCore(dbContext);
+
+                productFormatCore.Create(productFormat);
+                return Ok("Added new product-format successfully!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] ProductFormat productFormat, [FromRoute] int id)
+        {
+            try
+            {
+                ProductFormatCore productFormatCore = new ProductFormatCore(dbContext);
+
+                productFormatCore.Update(productFormat, id);
+                return Ok("Product-format successfully edited!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
