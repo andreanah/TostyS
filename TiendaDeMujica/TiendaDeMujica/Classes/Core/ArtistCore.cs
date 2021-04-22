99144731b89991afb0562c6bbb2ddc03c96ed2b4
AremZ <61175996+AremZ@users.noreply.github.com>
2021-04-22 13:33:50 -0500

    Agregado Controller y Core de Artist

---
+++ TiendaDeMujica/TiendaDeMujica/Classes/Core/ArtistCore.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaDeMujica.Models;
+
+namespace TiendaDeMujica.Classes.Core
+{
+    public class ArtistCore
+    {
+        TiendaDeMujicaDBContext dBContext;
+        public ArtistCore(TiendaDeMujicaDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public List<Artist> Get()
+        {
+            try
+            {
+                return (from a in dBContext.Artist
+                        where a.Active == true
+                        select a).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<Artist> Get(int id)
+        {
+            try
+            {
+                return (from a in dBContext.Artist
+                        where a.Active == true && a.Id == id
+                        select a).ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Create(Artist artist)
+        {
+            try
+            {
+                bool validArtist = Validate(artist);
+                if (validArtist)
+                {
+                    dBContext.Add(artist);
+                    dBContext.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public bool Validate(Artist artist)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(artist.StageName) || artist.StageName.Length > 50 ||
+                    string.IsNullOrEmpty(artist.RealName) || artist.RealName.Length > 50 ||
+                    string.IsNullOrEmpty(artist.Description) || artist.Description.Length > 50)
+                    return false;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Update(Artist artist, int id)
+        {
+            try
+            {
+                bool validArtist = Validate(artist);
+
+                if (validArtist)
+                {
+                    bool existingArtist = dBContext.Artist.Any(artist => artist.Id == id);
+                    if (existingArtist)
+                    {
+                        artist.Id = id;
+
+                        dBContext.Update(artist);
+                        dBContext.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Disable(int id)
+        {
+            try
+            {
+                Artist artist = dBContext.Artist.FirstOrDefault(x => x.Id == id && x.Active);
+                if (artist != null)
+                {
+                    artist.Active = false;
+                    dBContext.Update(artist);
+                    dBContext.SaveChanges();
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ TiendaDeMujica/TiendaDeMujica/Controllers/ArtistController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TiendaDeMujica.Classes.Core;
+using TiendaDeMujica.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TiendaDeMujica.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        private TiendaDeMujicaDBContext dbContext;
+
+        public ArtistController(TiendaDeMujicaDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                ArtistCore artistCore = new ArtistCore(dbContext);
+                return Ok(artistCore.Get());
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            try
+            {
+                ArtistCore artistCore = new ArtistCore(dbContext);
+                return Ok(artistCore.Get(id));
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Artist artist)
+        {
+            try
+            {
+                ArtistCore artistCore = new ArtistCore(dbContext);
+
+                artistCore.Create(artist);
+                return Ok("Added new artist successfully!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Artist artist, [FromRoute] int id)
+        {
+            try
+            {
+                ArtistCore artistCore = new ArtistCore(dbContext);
+
+                artistCore.Update(artist, id);
+                return Ok("Artist successfully edited!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Disable([FromRoute] int id)
+        {
+            try
+            {
+                ArtistCore artistCore = new ArtistCore(dbContext);
+
+                artistCore.Disable(id);
+                return Ok("Artist successfully disabled!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
