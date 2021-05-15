f7fe697342ac0056f1adf2336f132eb9acfe922b
Edgar Niño <edgarnio@live.com>
2021-05-14 19:06:10 -0500

    Se agrego la funcion ShoppingCartToOrder a ShoppingCart
    Sirve para pasar los productos del carrito de compras a orden

---
+++ TiendaDeMujica/TiendaDeMujica/Models/ViewModels/UserAndAddressModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaDeMujica.Models.ViewModels
+{
+    public class UserAndAddressModel
+    {
+        public string IdUser { get; set; }
+        public int IdAddress { get; set; }
+    }
+}
