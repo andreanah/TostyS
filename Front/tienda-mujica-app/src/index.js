f9df6afbf1fef6dee9c72ec575f1b5d2ff1f5264
Edgar Niño <edgarnio@live.com>
2021-05-20 00:03:40 -0500

    Se agregaron las alertas

--- Front/tienda-mujica-app/src/index.js
+++ Front/tienda-mujica-app/src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header';
+// import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
-import Login from './components/Login'
-import styled from '@emotion/styled'
+// import Login from './components/Login'
+// import styled from '@emotion/styled'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+const alertOptions = {
+  position: positions.BOTTOM_RIGHT,
+  timeout: 5000,
+  offset: "30px",
+  transitions: transitions.FADE,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    
-    <App/>
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
