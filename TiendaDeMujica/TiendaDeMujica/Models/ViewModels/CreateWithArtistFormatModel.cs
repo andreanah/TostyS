3611ffd656c3444d990e484489e1e10ea4541591
Edgar Niño <edgarnio@live.com>
2021-05-20 00:00:49 -0500

    Se creo el  viewmodel CreateWithArtistFormatModel

---
+++ TiendaDeMujica/TiendaDeMujica/Models/ViewModels/CreateWithArtistFormatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaDeMujica.Models.ViewModels
+{
+    public class CreateWithArtistFormatModel
+    {
+        public Product Product { get; set; }
+
+        public List<int> IdArtists { get; set; }
+        public List<int> IdFormats { get; set; }
+    }
+}
