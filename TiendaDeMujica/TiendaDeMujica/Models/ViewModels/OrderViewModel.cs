b4354c84f88976e219441a2f59d4eaa6f9fa1488
Edgar Niño <edgarnio@live.com>
2021-05-22 01:06:18 -0500

    OrderViewModel para funciones relacionadas a order

---
+++ TiendaDeMujica/TiendaDeMujica/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaDeMujica.Models.ViewModels
+{
+    [NotMapped]
+    public class OrderViewModel
+    {
+        public Order Order { get; set; }
+        public Address Address { get; set; }
+        public List<OrderProductViewModel> OrderProducts { get; set; }
+    }
+
+    [NotMapped]
+    public class OrderProductViewModel
+    {
+        public OrderProduct OrderProduct { get; set; }
+        public Format Format { get; set; }
+        public Product Product { get; set; }
+    }
+}
