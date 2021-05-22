af62ac7b81545344186ed853aa33d79696fca232
Edgar Niño <edgarnio@live.com>
2021-05-22 01:07:44 -0500

    Despliegue correcto de myOrders

---
+++ Front/tienda-mujica-app/src/api/OrderAPI.js
@@ -0,0 +1,23 @@
+﻿import { axiosBase as axios } from "./AxiosConfig"
+
+export const GetOrderOrderProducts = async (id) => {
+    try {
+        const response = await axios.get(`/order/GetOrderOrderProducts/${id}`)
+        console.log("GetOrderOrderProducts", response.data)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
+
+export const OrderConfirm = async (id) => {
+    try {
+        const response = await axios.put(`/order/OrderConfirm/${id}`)
+        console.log("OrderConfirm", response.data)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
--- Front/tienda-mujica-app/src/components/MyOrders.js
+++ Front/tienda-mujica-app/src/components/MyOrders.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -20,6 +20,14 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import { shadows } from '@material-ui/system';
+import ProductCard from '../components/Cards/ProductCard'
+
+import { useAlert } from 'react-alert'
+
+import { Identity } from '../api/UserAPI';
+import { GetProducts } from '../api/ProductAPI'
+
+import { GetOrderOrderProducts, OrderConfirm } from '../api/OrderAPI'
 
 function Copyright() {
   return (
@@ -40,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
       margin: 0,
       padding: 0,
       listStyle: 'none',
-      overflowY:'auto',
+      overflowY: 'auto',
     },
   },
   appBar: {
@@ -80,239 +88,174 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const tiers = [
-  {
-    title: 'Order 3',
-    subheader: 'Sent',
-    price: '50',
-    description: ['The Dark Side of the Moon', 'My Worlds', 'Use your Illusion ll', 'Awaken, My Love'],
-    buttonText: 'I got it',
-    buttonVariant: 'contained',
-  },
-  {
-    title: 'Order 2',
-    subheader: 'Delivered',
-    price: '100',
-    description: [
-      'High School Musical',
-      'Unorthodox Jukebox',
-      '22',
-      'Ye',
-    ],
-    buttonText: 'Delivered',
-    buttonVariant: 'outlined',
-  },
-  {
-    title: 'Order 1',
-    subheader: 'Delivered',
-    price: '30',
-    description: [
-      'Journals',
-      '20 Exitos',
-      'Garage Inc',
-      'Thrill Seeker',
-    ],
-    buttonText: 'Delivered',
-    buttonVariant: 'outlined',
-  },
-];
-
-
 export default function OrderElements() {
+  const numberCards = 4;
   const classes = useStyles();
+  const alert = useAlert();
+  const [products, setProducts] = useState([])
+  const [orderOrderProducts, setOrderOrderProducts] = useState()
+
+  useEffect(() => {
+    async function fetchData() {
+      var response = await Identity();
+      if (response.isAxiosError) {
+        return;
+      }
+      var id = response[0].id;
+
+      var response = await GetOrderOrderProducts(id);
+      if (!response.isAxiosError) {
+        setOrderOrderProducts(response)
+      }
+      var res = await GetProducts();
+      if (!res.isAxiosError) {
+        var productRes = res;
+        if (productRes.length > numberCards) {
+          productRes = productRes.slice(0, numberCards)
+        }
+        if (productRes.length < numberCards) {
+          for (let i = productRes.length; i < numberCards; i++) {
+            productRes.push(productRes[productRes.length - 1])
+          }
+        }
+        setProducts(productRes)
+      }
+
+    }
+    fetchData();
+  }, []);
+
+  const ConfirmOrder = async (id) => {
+    var res = await OrderConfirm(id);
+    if (!res.isAxiosError) {
+      alert.success("Se confirmo la orden");
+      window.location.reload(false);
+    } else {
+      alert.error("No se pudo confirmar la orden")
+    }
+  }
+
+  const CardsOrder = (OOP) => (
+    <Card>
+      <CardHeader
+        title={OOP?.title}
+        subheader={OOP?.subheader}
+        titleTypographyProps={{ align: 'center' }}
+        subheaderTypographyProps={{ align: 'center' }}
+        className={classes.cardHeader}
+      />
+      <CardContent>
+        <ul>
+          <Grid container my={2} xs={12}>
+            <Grid item xs={5}>
+              <Typography variant="h5">
+                Producto
+            </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Typography variant="h5">
+                Cantidad
+            </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5">
+                P. unitario
+            </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h5">
+                Precio
+            </Typography>
+            </Grid>
+          </Grid>
+          {OOP?.orderProducts?.map((OP, index) => (
+            <Grid key={index} mb={2} container style={{ width: '100%' }}>
+              <Grid item xs={1}>
+                <img src={OP.product.urlImage} style={{ width: "100%" }} />
+              </Grid>
+              <Grid item xs={4}>
+                <Typography align="left" variant="h4">
+                  {OP.product.name}
+                </Typography>
+                <Typography align="left" variant="h5">
+                  {OP.format.type}
+                </Typography>
+              </Grid>
+              <Grid item xs={1}>
+                <Typography align="center" variant="h5">
+                  {OP.orderProduct.quantity}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography align="center" variant="h5">
+                  ${OP.orderProduct.unitPrice}MXN
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography align="center" variant="h5">
+                  ${OP.orderProduct.totalPrice}MXN
+                </Typography>
+              </Grid>
+            </Grid>
+          ))}
+          <div className={classes.cardPricing}>
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography align="right" component="h4" variant="h4" color="textPrimary">
+                  Total: ${OOP?.order.total}MXN
+              </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography align="right" component="h6" variant="h6" color="textPrimary">
+                  {OOP?.address.street}, {OOP?.address.suburb}, {OOP?.address.cp}, {OOP?.address.city}, {OOP?.address.state}, {OOP?.address.country}
+                </Typography>
+              </Grid>
+            </Grid>
+          </div>
+        </ul>
+      </CardContent>
+      <CardActions>
+        <Button fullWidth disabled={(OOP?.order?.status === "Finalizado") ? true : false} onClick={() => ConfirmOrder(OOP?.order?.id)} variant={(OOP?.order?.status === "Finalizado") ? "outlined" : "contained"} color="primary">
+          {OOP?.order?.status}
+        </Button>
+      </CardActions>
+    </Card>
+  )
 
   return (
     <React.Fragment>
       <CssBaseline />
-   
-      <Header/>
-      <img alt="HeaderImage" src="/banner.png" width='100%'/>
+
+      <Header />
+      <img alt="HeaderImage" src="/banner.png" width='100%' />
       {/* End hero unit */}
       <Container maxWidth="md" component="main">
+        <Typography variant="h2" my={3}>
+          Mis ordenes
+        </Typography>
         <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
+          {orderOrderProducts?.map((orderOrderProduct) => (
             // Enterprise card is full width at sm breakpoint
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      /mo
-                    </Typography>
-                  </div>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography component="li" variant="subtitle1" align="center" key={line}>
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant} color="primary">
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
+            <Grid item key={orderOrderProducts.title} xs={12} sm={orderOrderProducts.title === 'Enterprise' ? 12 : 6} md={12}>
+              {CardsOrder(orderOrderProduct)}
             </Grid>
           ))}
         </Grid>
       </Container>
-      <img alt="HeaderImage" src="/recommendedforyou.png" width='100%'/>
-               
-      
-      <Grid container spacing={0.5} >
-      <Grid item xs>
-        <Card className={classes.root}>
-          
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                            Agregar al carrito
-                        </Button>
-            </CardActions>
-          
-        </Card>
-     </Grid>   
-     <Grid item xs>
-        <Card className={classes.root}>
-          
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                            Agregar al carrito
-                        </Button>
-            </CardActions>
-          
-        </Card>
-     </Grid>   
-     <Grid item xs>
-        <Card className={classes.root}>
-          
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                            Agregar al carrito
-                        </Button>
-            </CardActions>
-          
-        </Card>
-     </Grid>   
-     <Grid item xs>
-        <Card className={classes.root}>
-          
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                            Agregar al carrito
-                        </Button>
-            </CardActions>
-          
-        </Card>
-     </Grid> 
-     <Grid item xs>
-        <Card className={classes.root}>
-          
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" color="primary">
-                            Agregar al carrito
-                        </Button>
-            </CardActions>
-          
-        </Card>
-     </Grid>   
-       
-    </Grid>
+      <img alt="HeaderImage" src="/recommendedforyou.png" width='100%' />
 
-    <Footer/>      
-     
+      <Grid my={3} container spacing={5} alignItems="flex-">
+        {products.map((product, index) => (
+          <Fragment key={index}>
+            <Grid item xs={3} p={0}>
+              <ProductCard product={product} />
+            </Grid>
+          </Fragment>
+        ))}
+      </Grid>
+
+      <Footer />
+
     </React.Fragment>
   );
 }
